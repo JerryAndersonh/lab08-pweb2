894bf0c98926fabf52f7eafdfb137dcca6e2b2d4
JerryAndersonh <jhuaynacho@unsa.edu.pe>
2025-06-08 17:05:30 -0500

    modelos para many to many agregados

--- sistema/principal/models.py
+++ sistema/principal/models.py
@@ -26,3 +26,18 @@ class Framework(models.Model):
 
     def __str__(self):
         return self.name
+    
+
+#models para relations many to many
+class Movie(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Character(models.Model):
+    name = models.CharField(max_length=10)
+    movies = models.ManyToManyField(Movie)
+
+    def __str__(self):
+        return self.name
