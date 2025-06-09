a952fac8b2771bf63a4f815fce18e6d485d38e90
JerryAndersonh <jhuaynacho@unsa.edu.pe>
2025-06-08 21:06:02 -0500

    direccion agregada con contrasena de aplicacion

--- emailexample/emailexample/settings.py
+++ emailexample/emailexample/settings.py
@@ -121,3 +121,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'jhuaynacho@unsa.edu.pe'
+EMAIL_HOST_PASSWORD = 'orza vnfc fxzh vzqt'
+
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
--- emailexample/send/views.py
+++ emailexample/send/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
+
 
 def index(request):
+
+    send_mail('Hello from JerryAndersonMail', 
+    'Hello there, this is a test email from JerryAndersonMail.',
+    '959e87ce89652e',
+    ['dopox28863@adrewire.com'],
+    fail_silently=False)
+
     return render(request, 'send/index.html')
